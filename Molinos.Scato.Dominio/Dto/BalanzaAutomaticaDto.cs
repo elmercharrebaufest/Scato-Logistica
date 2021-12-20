d1f2b49be85474e5e6adaa07acd1466f875eb83d
Becker Portilla <bportilla@baufest.com>
2021-12-20 09:56:46 -0300

    First commit

---
+++ Molinos.Scato.Dominio/Dto/BalanzaAutomaticaDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Molinos.Scato.Dominio.Enums;
+using Molinos.Scato.Dominio.Recursos;
+
+namespace Molinos.Scato.Dominio.Dto
+{
+    public sealed class BalanzaAutomaticaDto
+    {
+        public int PuestoId { get; set; }
+        public string NombreBalanza { get; set; }
+        public bool PausaFullAuto { get; set; }
+        public bool EsVagon { get; set; }
+        public bool EsExpo { get; set; }
+        public string CamaraUrl { get; set; }
+        public string Camara { get; set; }
+        public int BalanzaId { get; set; }
+        public int? Orden { get; set; }
+    }
+}
+ 
---
+++ Molinos.Scato.Dominio/Dto/EstadoSensoresBalanzaDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Molinos.Scato.Dominio.Dto
+{
+    public class EstadoSensoresBalanzaDto
+    {
+        public bool BarreraEntradaActiva { get; set; }
+        public bool BarreraSalidaActiva { get; set; }
+        public bool SensorIngresoActiva { get; set; }
+        public bool SensorTrompaActiva { get; set; }
+        public int PuestoId{ get; set; }
+    }
+}
