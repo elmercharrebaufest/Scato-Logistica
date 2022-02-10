7ee6fedb8684abbbe605476bdfa32deccf39a73b
Becker Portilla <bportilla@baufest.com>
2022-02-09 23:52:07 -0300

    Fix puerto

--- Molinos.Scato.WebPuerto/src/app/shared/servicios/manosEmbarque.service.ts
+++ Molinos.Scato.WebPuerto/src/app/shared/servicios/manosEmbarque.service.ts
@@ -1,13 +1,14 @@
 import { EventEmitter, Injectable, Output } from '@angular/core';
 
+
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class ManosEmbarqueService {
-    @Output() removerManoDeEmbarque = new EventEmitter<object>();
-    @Output() removerResaltadoSilos = new EventEmitter();
-    @Output() resaltarSilo = new EventEmitter<string>();
-    @Output() agregarManoDeEmbarque = new EventEmitter<object>();
-    @Output() agregarTabique = new EventEmitter<object>();
-    @Output() removerTabique = new EventEmitter<string>();
+  @Output() removerManoDeEmbarque = new EventEmitter<object>();
+  @Output() removerResaltadoSilos = new EventEmitter<object>();
+  @Output() resaltarSilo = new EventEmitter<string>();
+  @Output() agregarManoDeEmbarque = new EventEmitter<object>();
+  @Output() agregarTabique = new EventEmitter<object>();
+  @Output() removerTabique = new EventEmitter<string>();
 }
