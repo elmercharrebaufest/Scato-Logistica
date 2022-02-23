d49f8584b20e103b2ed8f276a24c964d026556e1
Argenis Riera <ariera@baufest.com>
2022-02-23 10:03:06 -0300

    Merged PR 180: (SCT310-1680)FixPeso-Agregar Confirmación de Rechazo vagones
    (SCT310-1680)FixPeso-Agregar Confirmación de Rechazo vagones

--- Molinos.Scato.Actividades/Internas/VerificarCamionRechazado.cs
+++ Molinos.Scato.Actividades/Internas/VerificarCamionRechazado.cs
@@ -1,5 +1,8 @@
 using System.Activities;
+using Molinos.Scato.Dominio.Comandos;
 using Molinos.Scato.Dominio.Dto;
+using Molinos.Scato.Dominio.Enums;
+using Molinos.Scato.Servicios;
 
 namespace Molinos.Scato.Actividades.Internas
 {
@@ -12,10 +15,31 @@ namespace Molinos.Scato.Actividades.Internas
         public OutArgument<bool> RevierteRechazoVagon { get; set; }
         protected override void Execute(CodeActivityContext context)
         {
+            var servicio = context.GetExtension<IServicioComandos>();
             var controlRecorrido = ControlRecorrido.Get<ControlRecorridoDto>(context);
             Observacion.Set(context, controlRecorrido.Mensaje + "\n" + controlRecorrido.Comentario);
             Rechazar.Set(context, controlRecorrido.Decision);
             RevierteRechazoVagon.Set(context, controlRecorrido.RevierteRechazo);
+            if (controlRecorrido.RevierteRechazo)
+            {
+                servicio.Ejecutar(new ModificarRecorridoPeso
+                {
+                    InstanceId = context.WorkflowInstanceId,
+                    TipoPesada = TipoPesada.Bruto,
+                    Peso = null,
+                    BalanzaId = 0
+                });
+
+                servicio.Ejecutar(new ModificarRecorridoPeso
+                {
+                    InstanceId = context.WorkflowInstanceId,
+                    TipoPesada = TipoPesada.Tara,
+                    Peso = null,
+                    BalanzaId = 0,
+                    Usuario = controlRecorrido.NombreUsuario
+                });
+            }
+
         }
     }
 }
