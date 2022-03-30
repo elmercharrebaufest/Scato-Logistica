720f782689325dcbe023fdd9638d3cbf2bd02870
Damián Sánchez <dsanchez@baufest.com>
2022-03-30 17:49:03 -0300

    Merged PR 334: (ANS-999) Bug etapa camion demorado no granos
    (ANS-999) Bug etapa camion demorado no granos
    
    Fix de transportista en Camiones demorados No Granos

--- Molinos.Scato.Web/Controllers/CamionDemoradoController.cs
+++ Molinos.Scato.Web/Controllers/CamionDemoradoController.cs
@@ -200,7 +200,13 @@ namespace Molinos.Scato.Web.Controllers
                     IngresarOrdenCargaFasController.SetearVista(workflowObjt, servicio, this);
                     return View(orden);
                 }
-
+                var resultadoTransportista = SetearTransportista(ref transportistaId, orden.TipoComercialId, orden.EsTransportista);
+                orden.TransportistaId = transportistaId;
+                if (!resultadoTransportista)
+                {
+                    IngresarOrdenCargaFasController.SetearVista(workflowObjt, servicio, this);
+                    return View(orden);
+                }
                 var resultado = servicioComandos.Ejecutar(new ModificarOrdenCargaFas { Orden = orden, NombreUsuario = datosUsuario.NombreUsuario });
                 log.Debug($"({WorkflowId}) - { orden.PatenteCamion }: modificacion orden fas { orden.NumeroOrden} {(resultado.HayErrores? "con" : "sin")} error.");
 
