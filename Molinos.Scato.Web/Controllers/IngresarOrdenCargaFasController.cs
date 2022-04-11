67ea5b9ad64de648fb1b6ccc34e80083b9233fb5
Damián Sánchez <dsanchez@baufest.com>
2022-04-11 17:58:01 -0300

    Merged PR 389: (ANS-163) Workflow por exportacion fca
    (ANS-163) Workflow por exportacion fca
    
    Descomentar para la validacion de Expo o Venta FAS

--- Molinos.Scato.Web/Controllers/IngresarOrdenCargaFasController.cs
+++ Molinos.Scato.Web/Controllers/IngresarOrdenCargaFasController.cs
@@ -226,14 +226,16 @@ namespace Molinos.Scato.Web.Controllers
                         {
                             return Json(new { datosSap = -1, error = string.Format(Textos.OrdenCargaFAS_ClienteInexistente, ordenCargaFas[i].KUNAG) }, JsonRequestBehavior.AllowGet);
                         }
-                        //if (workflow.Contains("Venta") && !tipoComercial.Descripcion.ToLower().Contains("venta"))
-                        //{
-                        //    continue;
-                        //}
-                        //if (workflow.Contains("Expo") && !tipoComercial.Descripcion.ToLower().Contains("expo"))
-                        //{
-                        //    continue;
-                        //}
+                        if (workflow.Contains("Venta") && tipoComercial.Descripcion.ToLower().Contains("EFC"))
+                        {
+                            log.Debug($"{ordenCargaFas[i].VBELN} no es Venta fas y tiene tipo comercial {ordenCargaFas[i].TIPO_COMERCIAL}")
+                            continue;
+                        }
+                        if (workflow.Contains("Expo") && !tipoComercial.Descripcion.ToLower().Contains("EFC"))
+                        {
+                            log.Debug($"{ordenCargaFas[i].VBELN} no es Expo fas y tiene tipo comercial {ordenCargaFas[i].TIPO_COMERCIAL}")
+                            continue;
+                        }
                         var itemSap = new OrdenCargaFasDto
                         {
                             CuitTransporte = ConvertirCuil(ordenCargaFas[i].CUIT_TR),
