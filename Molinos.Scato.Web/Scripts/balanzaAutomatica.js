0fc84d13db4df7363c4b74342ffa685b839bad6e
Damián Sánchez <dsanchez@baufest.com>
2022-04-18 10:35:02 -0300

    Merged PR 404: (ANS-813) Revisar estado correcto de posición de lanza
    Ajuste de estado de posicion de lanza

--- Molinos.Scato.Web/Scripts/balanzaAutomatica.js
+++ Molinos.Scato.Web/Scripts/balanzaAutomatica.js
@@ -206,6 +206,7 @@ $(document).ready(function () {
     $(".btn-balanza").attr("disabled", true);  
     $(document).on('click', ".btn-modal-finalizar", AbrirModalFinalizarPesaje);
 
+    $.post($("#actualizarEstadoSensores").val());
 });
 
 function ValidarMotivo() {
