eec8b38655a3567e9f85c71d842b6fc29a93cca6
Oscar De Pierola <odepierola@baufest.com>
2022-04-25 17:30:58 -0500

    cambios de log4net para AI y de ErrorHandling

---
+++ Molinos.Scato.Web/ErrorHandler/AiHandleErrorAttribute.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.ApplicationInsights;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Molinos.Scato.Web.ErrorHandler
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class AiHandleErrorAttribute : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext != null && filterContext.HttpContext != null && filterContext.Exception != null)
+            {
+                //If customError is Off, then AI HTTPModule will report the exception
+                if (filterContext.HttpContext.IsCustomErrorEnabled)
+                {
+                    var ai = new TelemetryClient();
+                    ai.TrackException(filterContext.Exception);
+                }
+            }
+            base.OnException(filterContext);
+        }
+    }
+}
