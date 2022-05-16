55c6b840eaab0755aa5d0df4c4a0c78d09a78c8a
Oscar De Pierola <odepierola@baufest.com>
2022-05-16 08:30:01 -0500

    permitir nulos para color del material puerto

--- Molinos.Scato.Database/dbo/Tables/MaterialPuerto.sql
+++ Molinos.Scato.Database/dbo/Tables/MaterialPuerto.sql
@@ -5,7 +5,7 @@
     [CodigoSap]        NVARCHAR (50)  NULL,
     [Almacen_Id]       INT            NULL,
     [EsLiquido]        BIT            DEFAULT ((0)) NOT NULL,
-    [Color]            NVARCHAR (7)   DEFAULT ('#000000') NOT NULL,
+    [Color]            NVARCHAR (7)   DEFAULT ('#000000') NULL,
     CONSTRAINT [PK_MaterialPuerto] PRIMARY KEY CLUSTERED ([Id] ASC) WITH (STATISTICS_NORECOMPUTE = ON),
     CONSTRAINT [FK_dbo.MaterialPuerto_dbo.Almacen_Almacen_Id] FOREIGN KEY ([Almacen_Id]) REFERENCES [dbo].[Almacen] ([Id])
 );
