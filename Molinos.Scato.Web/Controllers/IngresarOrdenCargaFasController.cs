5b166434c820535ec5b6b26839840e67fb43c2fd
Oscar De Pierola <odepierola@baufest.com>
2022-05-23 12:49:34 -0500

    numeroDocumentoChofer

--- Molinos.Scato.Web/Controllers/IngresarOrdenCargaFasController.cs
+++ Molinos.Scato.Web/Controllers/IngresarOrdenCargaFasController.cs
@@ -216,7 +216,7 @@ namespace Molinos.Scato.Web.Controllers
                                 var endPos = numeroDocumentoChofer.LastIndexOf("-");
                                 numeroDocumentoChofer = numeroDocumentoChofer.Substring(startPos + 1, endPos - startPos - 1);
                             }
-                            else
+                            else if (numeroDocumentoChofer.Length >= 8)
                             {
                                 numeroDocumentoChofer = numeroDocumentoChofer.Substring(2, 8);
                             }
