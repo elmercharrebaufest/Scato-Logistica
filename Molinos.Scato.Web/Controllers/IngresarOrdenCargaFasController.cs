de02648eefcbf5de02a935435cbed712f587ef86
Manuel Dávila <mdavila@baufest.com>
2022-06-06 19:01:23 -0300

    Merged PR 673: SCT310-2152 - ANS-1356 - Camion no ingresado en SAP
    SCT310-2152 - ANS-1356 - Camion no ingresado en SAP

--- Molinos.Scato.Web/Controllers/IngresarOrdenCargaFasController.cs
+++ Molinos.Scato.Web/Controllers/IngresarOrdenCargaFasController.cs
@@ -241,7 +241,7 @@ namespace Molinos.Scato.Web.Controllers
                         {
                             return Json(new { datosSap = -1, error = string.Format(Textos.OrdenCargaFAS_ClienteInexistente, ordenCargaFas[i].KUNAG) }, JsonRequestBehavior.AllowGet);
                         }
-                        if (workflow.Contains("Venta") && tipoComercial != null && !tipoComercial.CodigoSap.Equals("998"))
+                        if (workflow.Contains("Venta") && tipoComercial != null && !(tipoComercial.CodigoSap.Equals("998") || tipoComercial.CodigoSap.Equals("CYO")))
                         {
                             log.Debug($"{ordenCargaFas[i].VBELN} no es Venta fas y tiene tipo comercial {ordenCargaFas[i].TIPO_COMERCIAL}");
                             continue;
