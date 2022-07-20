8b7a562f50008372a05fd2a07dfd2a51961750d0
Oscar De Pierola <odepierola@baufest.com>
2022-07-20 15:21:49 -0500

    (ANS-1536) Listado de Romaneo

--- Molinos.Scato.Web/Scripts/MvcReportViewer.js
+++ Molinos.Scato.Web/Scripts/MvcReportViewer.js
@@ -1,55 +1,109 @@
 ﻿(function () {
-    // ASP.NET Report Viewer Web Control Enhancements
+	// ASP.NET Report Viewer Web Control Enhancements
 
-    $(document).ready(function () {
-        // Detect is current browser is IE (MSIE is not used since IE 11)
-        var isIE = /MSIE/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent);
+	$(document).ready(function () {
+		// Detect is current browser is IE (MSIE is not used since IE 11)
+		var isIE = /MSIE/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent);
 
-        function viewerPropertyChanged(sender, e) {
-            var viewer = $find("ReportViewer");
+		function viewerPropertyChanged(sender, e) {
+			var viewer = $find("ReportViewer");
 
-            if (e.get_propertyName() === "isLoading" && !viewer.get_isLoading()) {
-                var reportViewerHeight = $('#ReportViewer').height();
-                var $uiRows = $('#ReportViewer_fixedTable > tbody > tr');
-                var controlsHeight = 0;
-                $uiRows.each(function (i, el) {
-                    if (i !== $uiRows.length - 1) {
-                        controlsHeight += $(el).height();
-                    }
-                });
+			if (e.get_propertyName() === "isLoading" && !viewer.get_isLoading()) {
+				var reportViewerHeight = $('#ReportViewer').height();
+				var $uiRows = $('#ReportViewer_fixedTable > tbody > tr');
+				var controlsHeight = 0;
+				$uiRows.each(function (i, el) {
+					if (i !== $uiRows.length - 1) {
+						controlsHeight += $(el).height();
+					}
+				});
 
-                var contentAreaHeight = reportViewerHeight - controlsHeight;
-                $('#VisibleReportContentReportViewer_ctl09').height(contentAreaHeight);
-            }
-        }
+				var contentAreaHeight = reportViewerHeight - controlsHeight;
+				$('#VisibleReportContentReportViewer_ctl09').height(contentAreaHeight);
+			}
+		}
 
-        function printReport() {
-            $find('ReportViewer').exportReport('PDF');
-        }
+		function printReport() {
+			$find('ReportViewer').exportReport('PDF');
+		}
 
-        // 1. Fix Report Scrolling in IE 10 and IE 11
-        if (window.hasUserSetHeight && isIE) {
-            Sys.Application.add_load(function () {
-                var reportViewer = $find("ReportViewer");
-                reportViewer.add_propertyChanged(viewerPropertyChanged);
-            });
-        }
+		// 1. Fix Report Scrolling in IE 10 and IE 11
+		if (window.hasUserSetHeight && isIE) {
+			Sys.Application.add_load(function () {
+				var reportViewer = $find("ReportViewer");
+				reportViewer.add_propertyChanged(viewerPropertyChanged);
+			});
+		}
 
-        // 2. Add Print button for non-IE browsers
-        if (!isIE && window.showPrintButton) {
-            var buttonHtml = $('#non-ie-print-button').html();
-            $('#ReportViewer_ctl05 > div').append(buttonHtml);
-            $('#PrintButton').click(function(e) {
-                e.preventDefault();
-                printReport();
-            });
+		// 2. Add Print button for non-IE browsers
+		if (!isIE && window.showPrintButton) {
+			var buttonHtml = $('#non-ie-print-button').html();
+			$('#ReportViewer_ctl05 > div').append(buttonHtml);
+			$('#PrintButton').click(function (e) {
+				e.preventDefault();
+				printReport();
+			});
 
-            $('#mvcreportviewer-btn-print').hover(function () {
-                $(this).css('cursor', 'pointer').css('border', '1px solid rgb(51, 102, 153)').css('background-color', 'rgb(221, 238, 247)');
-            }, function () {
-                $(this).css('cursor', 'pointer').css('border', '1px solid transparent').css('background-color', 'transparent');
-            });
-        }
-    });
-    
+			$('#mvcreportviewer-btn-print').hover(function () {
+				$(this).css('cursor', 'pointer').css('border', '1px solid rgb(51, 102, 153)').css('background-color', 'rgb(221, 238, 247)');
+			}, function () {
+				$(this).css('cursor', 'pointer').css('border', '1px solid transparent').css('background-color', 'transparent');
+			});
+		}
+
+		// FIX DE FECHAS PARA SCATO
+		if (!isIE) { 
+			var observer = new MutationObserver(function (mutations) {
+				var changes = false;
+				mutations.forEach(function (mutation) {
+					if (mutation.removedNodes.length > 0 && changes == false) {
+						applyDateFix()
+						changes = true;
+					}
+				})
+			});
+			observer.observe($("#reportForm")[0], { childList: true, subtree: true });
+			applyDateFix();
+
+			function applyDateFix() {
+				let datePickers = document.getElementsByClassName('null');
+				[...datePickers].forEach((element, index, array) => {
+					let elemVal = element.value;
+					if (elemVal != null && elemVal != "") {
+						if (!isValidDate(elemVal)) {
+							let newElemVal = changeDateFormat(elemVal);
+							element.value = newElemVal;
+						}
+					}
+					element.type = "date";
+				});
+			}
+
+
+			function isValidDate(dateString) {
+				let regEx = /^\d{4}-\d{2}-\d{2}$/;
+				if (!dateString.match(regEx)) return false;
+				let d = new Date(dateString);
+				let dNum = d.getTime();
+				if (!dNum && dNum !== 0) return false;
+				return d.toISOString().slice(0, 10) === dateString;
+			}
+
+			function changeDateFormat(date) {
+				date = date.split(" ")[0];
+				let dateArray = date.split('/');
+
+				for (var i = 0; i < dateArray.length; i++) {
+					if (dateArray[i].length < 2) {
+						dateArray[i] = "0" + dateArray[i];
+					}
+				}
+
+				const [day, month, year] = dateArray;
+				const result = [year, month, day].join('-');
+				return result;
+			}
+		}
+	});
+
 })();
