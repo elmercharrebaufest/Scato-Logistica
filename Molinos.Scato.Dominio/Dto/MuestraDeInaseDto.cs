5de16224cdf1c54f34a98f85ef315c7fada0b987
Manuel Dávila <mdavila@baufest.com>
2022-08-12 13:32:57 -0300

    Merged PR 912: SCT310-2278 - ANS-1623 - Agregar info mail Inase
    SCT310-2278 - ANS-1623 - Agregar info mail Inase

--- Molinos.Scato.Dominio/Dto/MuestraDeInaseDto.cs
+++ Molinos.Scato.Dominio/Dto/MuestraDeInaseDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Molinos.Scato.Dominio.Enums;
+using System;
 using System.Runtime.Serialization;
 
 namespace Molinos.Scato.Dominio.Dto
@@ -10,7 +11,33 @@ namespace Molinos.Scato.Dominio.Dto
         public int RecorridoId { get; set; }
         public Guid WorkflowInstanceId { get; set; }
         public int CentroId { get; set; }
-        public DateTime Fecha { get; set; }
+        public DateTime FechaMuestra { get; set; }
         public string CartaPorte { get; set; }
+        public string Patente { get; set; }
+        public string MaterialDescripcion { get; set; }
+        public string DestinatarioCuil { get; set; }
+        public string RtteComercialCuit { get; set; }
+        public string CorredorCuil { get; set; }
+        public int PesoNeto { get; set; }
+        public string TitularCartaPorte { get; set; }
+        public bool? CPE { get; set; }
+        public int? Sucursal { get; set; }
+        public string CTG { get; set; }
+        public string TitularCartaPorteCuil { get; set; }
+        public string CodEstab { get; set; }
+        public string Direccion { get; set; }
+        public string ProcedenciaCodigoSap { get; set; }
+        public string LocalidadCodigoSap { get; set; }
+        public TipoVehiculo TipoVehiculo { get; set; }
+        public int CantidadDeVagones { get; set; }
+        public string CodigoEstablecimiento { get; set; }
+        public string Corredor { get; set; }
+        public string IntermediarioCuit { get; set; }
+        public string Intermediario { get; set; }
+        public string RtteComercial { get; set; }
+        public string Cosecha { get; set; }
+        public int? ProcedenciaCodigoPostal { get; set; }
+        public int? ProcedenciaSubcodigoPostal { get; set; }
+        public DateTime FechaDescarga { get; set; }
     }
 }
---
+++ Molinos.Scato.Web/Models/ArchivosTxt/MuestraInase.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Molinos.Scato.Web.Models.ArchivosTxt
+{
+    public class MuestraInase
+    {
+        [TxtColumna(Order = 3, Longitud = 25)]
+        public string NombreProducto { get; set; }
+        [TxtColumna(Order = 4, Longitud = 11)]
+        public long CuitDestinatario { get; set; }
+        [TxtColumna(Order = 5, Longitud = 11)]
+        public long CuitRtteComercial { get; set; }
+        [TxtColumna(Order = 6, Longitud = 11)]
+        public long CuitCorredor { get; set; }
+        [TxtColumna(Order = 7, Longitud = 1)]
+        public long CodigoPagador { get; set; }
+        [TxtColumna(Order = 8, Longitud = 4)]
+        public int CodigoProcedencia { get; set; }
+        [TxtColumna(Order = 9, Longitud = 2)]
+        public int SubCodigoProcedencia { get; set; }
+        [TxtColumna(Order = 11, Longitud = 9)]
+        public long PesoNetoSeco { get; set; }
+        [TxtColumna(Order = 12, Longitud = 1)]
+        public string Lacrada { get; set; }
+        [TxtColumna(Order = 15, Longitud = 6)]
+        public DateTime FechaDescarga { get; set; }
+        [TxtColumna(Order = 18, Longitud = 1)]
+        public string ServicioLacrado { get; set; }
+        [TxtColumna(Order = 19, Longitud = 8)]
+        public string Patente { get; set; }
+        [TxtColumna(Order = 20, Longitud = 40)]
+        public string RtteComercial { get; set; }
+        [TxtColumna(Order = 24, Longitud = 13)]
+        public long CartaDePorte { get; set; }
+        [TxtColumna(Order = 25, Longitud = 12)]
+        public long NumeroCTG { get; set; }
+        [TxtColumna(Order = 26, Longitud = 11)]
+        public long CuitTitularCartaPorte { get; set; }
+        [TxtColumna(Order = 27, Longitud = 40)]
+        public string TitularCartaPorte { get; set; }
+        [TxtColumna(Order = 29, Longitud = 40)]
+        public string Establecimiento { get; set; }
+        [TxtColumna(Order = 30, Longitud = 60)]
+        public string DireccionPostalDestino { get; set; }
+        [TxtColumna(Order = 31, Longitud = 5)]
+        public int CodigoLocalidadONCCAProcedencia { get; set; }
+        [TxtColumna(Order = 32, Longitud = 5)]
+        public int CodigoLocalidadONCCADestino { get; set; }
+        [TxtColumna(Order = 33, Longitud = 2)]
+        public string TipoDeTransporte { get; set; }
+        [TxtColumna(Order = 34, Longitud = 2)]
+        public int CantidadVagones { get; set; }
+        [TxtColumna(Order = 35, Longitud = 15)]
+        public string IdentificadorVagon { get; set; }
+        [TxtColumna(Order = 36, Longitud = 10)]
+        public long CodigoPlantaONCCADestino { get; set; }
+        [TxtColumna(Order = 37, Longitud = 40)]
+        public string RazonSocialCorredor { get; set; }
+        [TxtColumna(Order = 38, Longitud = 11)]
+        public long CuitIntermediario { get; set; }
+        [TxtColumna(Order = 39, Longitud = 40)]
+        public string RazonSocialIntermediario { get; set; }
+        [TxtColumna(Order = 40, Longitud = 11)]
+        public long CuitRepresentante { get; set; }
+        [TxtColumna(Order = 41, Longitud = 40)]
+        public string RazonSocialRepresentante { get; set; }
+        [TxtColumna(Order = 42, Longitud = 4)]
+        public long Cosecha { get; set; }
+
+    }
+}
