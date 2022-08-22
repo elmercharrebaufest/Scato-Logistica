8dc6804beff3fd9b3d33782bb5d88f1a297e329a
Oscar De Pierola <odepierola@baufest.com>
2022-08-22 12:24:49 -0500

    add mapping profile

---
+++ Molinos.Scato.Servicios/Conversiones/Impl/Perfiles/LogDispositivoMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Molinos.Scato.Dominio.Dto;
+using Molinos.Scato.Dominio.Entidades;
+
+namespace Molinos.Scato.Servicios.Conversiones.Impl.Perfiles
+{
+    public class LogDispositivoMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "LogDispositivoMappingProfile"; }
+        }
+        protected override void Configure()
+        {
+            Mapper.CreateMap<LogDispositivo, LogDispositivoDto>();
+            Mapper.CreateMap<LogDispositivoDto, LogDispositivo>();
+        }
+    }
+}
