7fcaa2b41c138ca3adc570ff77dc289ef59a9047
Oscar De Pierola <odepierola@baufest.com>
2022-08-29 10:08:03 -0500

    rename de CTG a Numero Carta Orden

--- Molinos.Scato.Dominio/Comandos/ConsultarAFIP.cs
+++ Molinos.Scato.Dominio/Comandos/ConsultarAFIP.cs
@@ -4,6 +4,6 @@
     {
         public int CentroId { get; set; }
         public int TipoVehiculoId { get; set; }
-        public string CTG { get; set; }
+        public string NumeroCartaOrden { get; set; }
     }
 }
