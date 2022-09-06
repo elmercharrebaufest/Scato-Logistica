fd12ce5ae725af83e6ec0522b09238f8c3498cd2
Oscar Alessandro De Pierola Ardela <odepierola@baufest.com>
2022-09-06 13:42:02 -0300

    Merged PR 1046: SCT310-2435-(ANS-1626)-Logica-Redondeo-Informe-Ponderado-x-Característica-vs-SAP

--- Molinos.Scato.Actividades/Internas/IngresosPorCompraDeGranosGenerarRequest.cs
+++ Molinos.Scato.Actividades/Internas/IngresosPorCompraDeGranosGenerarRequest.cs
@@ -258,7 +258,7 @@ namespace Molinos.Scato.Actividades.Internas
                                         TRANSPORTISTA = cartaPorte?.TransportistaCUIT?.Replace("-", "") ?? string.Empty,
                                         VARIEDAD = cartaPorte?.Variedad,
                                         CORREDOR = cartaPorte.Cpe ? string.IsNullOrEmpty(corredorSAP) ? string.Empty : PadProveedor(corredorSAP) : PadProveedor(cartaPorte?.CorredorCodigoSap),
-                                        NETO_DESCONTADO = ((int)decimal.Round(kilosNetosDescontados)).ToString(CultureInfo.InvariantCulture),
+                                        NETO_DESCONTADO = ((int)decimal.Round(kilosNetosDescontados,MidpointRounding.AwayFromZero)).ToString(CultureInfo.InvariantCulture),
                                         CUENTAORDEN = firmasCuit.Any(x => x == cartaPorte.DestinatarioCuil) ? (rtteComercialSAP != null ? PadProveedor(rtteComercialSAP ): string.Empty ): PadProveedor(cartaPorte.DestinatarioCodigoSap),
                                         HORA_BRUTO = fechaPesoBruto.ToString("HHmmss", CultureInfo.InvariantCulture),
                                         HORA_CALADO = calado.FechaCreacion.Value.ToString("HHmmss", CultureInfo.InvariantCulture),
