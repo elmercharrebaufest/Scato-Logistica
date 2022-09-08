c92093ae2afc70fd7935087a083842c9906f745f
Oscar Alessandro De Pierola Ardela <odepierola@baufest.com>
2022-09-08 18:15:57 -0300

    Merged PR 1074: SCT310-2377-(ANS-1593)-Análisis-ticket-WF-Venta-SLO
    SCT310-2377-(ANS-1593)-Análisis-ticket-WF-Venta-SLO

--- Molinos.Scato.Dominio/Recursos/Textos.Designer.cs
+++ Molinos.Scato.Dominio/Recursos/Textos.Designer.cs
@@ -19,7 +19,7 @@ namespace Molinos.Scato.Dominio.Recursos {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Textos {
@@ -29672,6 +29672,15 @@ namespace Molinos.Scato.Dominio.Recursos {
         public static string Workflow_RechazoEtapa {
             get {
                 return ResourceManager.GetString("Workflow_RechazoEtapa", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to IngresarCartaPorteRedespachoMercaderia.
+        /// </summary>
+        public static string Workflow_SalidaMercaderiaConSAP {
+            get {
+                return ResourceManager.GetString("Workflow_SalidaMercaderiaConSAP", resourceCulture);
             }
         }
         
