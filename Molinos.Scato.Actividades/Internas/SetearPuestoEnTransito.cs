910d2bf198a715f974ed79d90b71bb2d5fa65c87
Manuel Dávila <mdavila@baufest.com>
2022-10-26 14:17:45 -0300

    Merged PR 1223: SCT310-2590 - ANS-1800 - Logica y Visualizacion en Puesto 2
    SCT310-2590 - ANS-1800 - Logica y Visualizacion en Puesto 2

---
+++ Molinos.Scato.Actividades/Internas/SetearPuestoEnTransito.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Activities;
+using Molinos.Scato.Dominio.Comandos;
+using Molinos.Scato.Dominio.Dto;
+using Molinos.Scato.Dominio.Recursos;
+using Molinos.Scato.Servicios;
+
+namespace Molinos.Scato.Actividades.Internas
+{
+    public class SetearPuestoEnTransito : CodeActivity<Resultado>
+    {
+        [RequiredArgument]
+        public InArgument<ControlRecorridoDto> ControlRecorridoDto { get; set; }
+
+        public OutArgument<int> PuestoDeTrabajoEnTransitoId { get; set; }
+
+        protected override Resultado Execute(CodeActivityContext context)
+        {
+            var resultado = new Resultado();
+            var controlRecorrido = ControlRecorridoDto.Get<ControlRecorridoDto>(context);
+            if (controlRecorrido != null)
+            {
+                PuestoDeTrabajoEnTransitoId.Set(context, controlRecorrido.PuestoDeTrabajoId);
+            }
+            return resultado;
+        }
+    }
+}
