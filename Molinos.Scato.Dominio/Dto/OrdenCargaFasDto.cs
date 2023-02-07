8681cad5b183a761a63e87e9162d1c9f3fe2b3c4
Manuel Dávila <mdavila@baufest.com>
2023-02-07 13:52:40 -0300

    Merged PR 1552: SCT310-2807 - ANS-1859 - Workflow Venta FAS

--- Molinos.Scato.Dominio/Dto/OrdenCargaFasDto.cs
+++ Molinos.Scato.Dominio/Dto/OrdenCargaFasDto.cs
@@ -83,16 +83,16 @@ namespace Molinos.Scato.Dominio.Dto
         public string MotivoDemora { get; set; }
         public bool ActualizarTipoVehiculo { get; set; }
 
-        [Display(ResourceType = typeof(Textos), Name = "OrdenCargaInterna_DerivadoGranarioHabilitado")]
+        [Display(ResourceType = typeof(Textos), Name = "OrdenCarga_DerivadoGranarioHabilitado")]
         public bool DerivadoGranarioHabilitado { get; set; }
 
-        [Display(ResourceType = typeof(Textos), Name = "OrdenCargaInterna_PlantaDGDestino")]
+        [Display(ResourceType = typeof(Textos), Name = "OrdenCarga_PlantaDGDestino")]
         public int? PlantaDGDestino { get; set; }
 
-        [Display(ResourceType = typeof(Textos), Name = "OrdenCargaInterna_OrdenDomicilioDestino")]
+        [Display(ResourceType = typeof(Textos), Name = "OrdenCarga_OrdenDomicilioDestino")]
         public int? OrdenDomicilioDestino { get; set; }
 
-        [Display(ResourceType = typeof(Textos), Name = "OrdenCargaInterna_CuitPagadorFlete")]
+        [Display(ResourceType = typeof(Textos), Name = "OrdenCarga_CuitPagadorFlete")]
         public string PagadorFlete { get; set; }
         public int? PagadorFleteId { get; set; }
         public bool Inhabilitado { get; set; }
