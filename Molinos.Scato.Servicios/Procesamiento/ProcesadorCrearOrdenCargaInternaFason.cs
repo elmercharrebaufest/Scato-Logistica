76a2abb1bbb98c43bc8b52edec4d7f87e9f8e2bc
Camila Bouza <cbouza@baufest.com>
2023-03-03 19:53:37 -0300

    Merged PR 1701: SCT310-3071-(ANS-2374)-Agregar-Campo-Intermediario-Flete-para-CPEDG-(Wfs-Fason-y-Fason-sin-Flete)

--- Molinos.Scato.Servicios/Procesamiento/ProcesadorCrearOrdenCargaInternaFason.cs
+++ Molinos.Scato.Servicios/Procesamiento/ProcesadorCrearOrdenCargaInternaFason.cs
@@ -58,7 +58,7 @@ namespace Molinos.Scato.Servicios.Procesamiento
                         Corredor = Repositorio.Obtener<Proveedor>(comando.Orden.CorredorId),
                         Comisionista = Repositorio.Obtener<Cliente>(comando.Orden.ComisionistaId),
                         Remitente = Repositorio.Obtener<Cliente>(comando.Orden.RemitenteId),
-                        IntermediarioFlete = Repositorio.Obtener<Proveedor>(comando.Orden.IntermediarioFlete),
+                        IntermediarioFlete = Repositorio.Obtener<Proveedor>(comando.Orden.IntermediarioFleteId),
                         TipoDomicilioDestino = comando.Orden.TipoDomicilioDestino,
                     };
 
