491fdc8f0ed00fe42790bb03dd73083bd4a63841
Oscar Alessandro De Pierola Ardela <odepierola@baufest.com>
2023-03-06 12:54:30 -0300

    Merged PR 1706: MERGE-SCT310-3076-(ANS-000)-Agregar-Domicilios-Tipo-Uno
    MERGE-SCT310-3076-(ANS-000)-Agregar-Domicilios-Tipo-Uno

--- Molinos.Scato.Web/Scripts/ordencargainterna.index.js
+++ Molinos.Scato.Web/Scripts/ordencargainterna.index.js
@@ -153,6 +153,10 @@
         ValidarDerivadoGranario();
     });
 
+    $('#TipoYOrdenDestino').change(function () {
+        $('#TipoYOrdenDestino').attr('title', $('#TipoYOrdenDestino :selected').text());
+    });
+
     if ($('#DestinoId').length > 0 || $('#ClienteId').length > 0) {
         CargarPlantas();
         CargarDomicilios();
@@ -498,6 +502,7 @@ function CargarDomicilios() {
                         && tipoDomicilioSeleccionado.length > 0
                         && allData.Domicilios.some(domicilio => domicilio.Orden == ordenDomicilioSeleccionado && domicilio.Tipo == tipoDomicilioSeleccionado)) {
                         $('#TipoYOrdenDestino').val(`${tipoDomicilioSeleccionado}-${ordenDomicilioSeleccionado}`)
+                        $('#TipoYOrdenDestino').attr('title', $('#TipoYOrdenDestino :selected').text());
                     }
                 }
 
