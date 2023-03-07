690e47e0bf1d7a3a62dd631819b8333dd6d4c313
mdavila <mdavila@baufest.com>
2023-03-07 10:38:57 -0500

    SCT310-3040 - ANS-000 - Permitir ingreso remito generico

--- Molinos.Scato.Web/Controllers/CargarOrdenDeDescargaFasonController.cs
+++ Molinos.Scato.Web/Controllers/CargarOrdenDeDescargaFasonController.cs
@@ -92,8 +92,7 @@ namespace Molinos.Scato.Web.Controllers
 
             var ordenDeDescargaPorRemito = servicio.ObtenerOrdenDeDescargaFasonPorNumeroRemito(orden.NumeroRemito);
 
-            //if (ordenDeDescargaPorRemito != null && orden.NumeroRemito != Constantes.ValoresPorDefecto.NumeroRemitoGenerico)
-            if (ordenDeDescargaPorRemito != null)
+            if (ordenDeDescargaPorRemito != null && orden.NumeroRemito != Constantes.ValoresPorDefecto.NumeroRemitoGenerico)
             {
                 ModelState.AddModelError("NumeroRemito", Textos.IngresarRemito_Existente);
                 SetearVista(workflowObj, datosUsuario.CentroId);
