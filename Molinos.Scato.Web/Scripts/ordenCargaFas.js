73e1cff0a6ec4fb0f877cf4209696346be3d5363
Alexis Vega <amorales@baufest.com>
2023-03-14 18:37:09 -0300

    Merged PR 1743: SCT310-3065-Error-Boton aceptar
    SCT310-3065-Error-Boton aceptar
    
    Se soluciona problema cuando se da click en boton rechazar, sin tener todos los campos requeridos y despues click en boton aceptar

--- Molinos.Scato.Web/Scripts/ordenCargaFas.js
+++ Molinos.Scato.Web/Scripts/ordenCargaFas.js
@@ -139,15 +139,22 @@ jQuery(document).ready(function () {
             $("#VehiculoDemorado").val("False");
         }
 
-        if ($("#Rechazado").length > 0) {
-            $("#Rechazado").val("True");
-        }
-
         if (valido) {
+            $("#Rechazado").val("True");
             modalRechazarOrdenCargaFas.close();
             $("#ordenCargaFas-form").submit();
+           
         }
     })
+
+    $("#btn-aceptar").click(function () {
+        
+            $("#Rechazado").val("False");
+            $("#ordenCargaFas-form").submit();
+
+    })
+
+
 });
 
 function deshabilitarKmRecorrerYLocalidad() {
