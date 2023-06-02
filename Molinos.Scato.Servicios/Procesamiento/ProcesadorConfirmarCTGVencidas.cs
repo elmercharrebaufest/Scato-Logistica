eca97cd59226fa449f2890bd59a44069a8db4e06
Manuel Dávila <mdavila@baufest.com>
2023-06-02 14:51:48 -0300

    Merged PR 2021: SCT310-3334 - ANS-2489 - Confirmacion automatica CTG

--- Molinos.Scato.Servicios/Procesamiento/ProcesadorConfirmarCTGVencidas.cs
+++ Molinos.Scato.Servicios/Procesamiento/ProcesadorConfirmarCTGVencidas.cs
@@ -38,14 +38,12 @@ namespace Molinos.Scato.Servicios.Procesamiento
             try
             {
                 var client = GenerarClienteWebAPI();
-                Log.Debug("ProcesadorConfirmarCTGVencidas - Se generó cliente web API");
                 var ctgsVencidos = ObtenerCTGVencidos(comando, client);
-                Log.Debug($"ProcesadorConfirmarCTGVencidas - Se obtuvo CTGs vencidos {ctgsVencidos.ToJson()}");
+                Log.Debug($"CTGDG Vencidos: {ctgsVencidos.ToJson()}");
                 foreach (var ctg in ctgsVencidos)
                 {
                     var resultadoCpe = ObtenerDatosCPEDG(comando.CentroId, ctg);
-                    Log.Debug($"ProcesadorConfirmarCTGVencidas - Se obtuvo CTGs vencidos NroCTG:{resultadoCpe.NroCTG} Sucursal:{resultadoCpe.Sucursal} NroOrden:{resultadoCpe.NroOrden} TipoCPE:{resultadoCpe.TipoCPE} Patente:{resultadoCpe.PatenteCamion}");
-                    if (!resultadoCpe.HayErrores)
+                    if(!resultadoCpe.HayErrores)
                         ConfirmarCPEDescargadoEnDestino(client, resultadoCpe);
                 }
             }
@@ -81,8 +79,6 @@ namespace Molinos.Scato.Servicios.Procesamiento
 
                 if (responseWebAPI.IsSuccessful)
                     resultado = JsonConvert.DeserializeObject<ResponseWebAPIDto<List<ResultadoCPEPendienteResolucion>>>(responseWebAPI.Content);
-
-                Log.Debug($"Resultado ObtenerCTGVencidos: {resultado.ToJson()}");
 
                 if (resultado.IsValid)
                     ctgs.AddRange(resultado.Data.Where(x => x.NroCTG.ToString().StartsWith("3")).Select(x => x.NroCTG));
