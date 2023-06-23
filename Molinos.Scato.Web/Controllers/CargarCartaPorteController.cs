96a813968c83146a6222ca09d8046819bb0efa3d
Oscar Alessandro De Pierola Ardela <odepierola@baufest.com>
2023-06-23 19:22:41 -0300

    Merged PR 2089: SCT310-3535-ANS-940-Control-Obligatorio-WORKFLOW-Salidas-con-SAP-y-SIN-SAP
    SCT310-3535-ANS-940-Control-Obligatorio-WORKFLOW-Salidas-con-SAP-y-SIN-SAP

--- Molinos.Scato.Web/Controllers/CargarCartaPorteController.cs
+++ Molinos.Scato.Web/Controllers/CargarCartaPorteController.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Drawing;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Web.Mvc;
-using Molinos.Scato.Actividades.Interfaces;
+﻿using Molinos.Scato.Actividades.Interfaces;
 using Molinos.Scato.Actividades.Servicios;
 using Molinos.Scato.Dominio;
 using Molinos.Scato.Dominio.Comandos;
-using Molinos.Scato.Dominio.Consultas;
 using Molinos.Scato.Dominio.Dto;
 using Molinos.Scato.Dominio.Enums;
 using Molinos.Scato.Dominio.Helpers;
@@ -24,6 +15,13 @@ using Molinos.Scato.Web.Filtros;
 using Molinos.Scato.Web.Helpers;
 using Molinos.Scato.Web.Models;
 using Ninject.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace Molinos.Scato.Web.Controllers
 {
@@ -144,6 +142,12 @@ namespace Molinos.Scato.Web.Controllers
                 ModelState.Remove("CTG");
             }
 
+            if (workflow != "1035- SalidaDeMercaderíaConSAP" && workflow != "1035- SalidaDeMercaderíaSinSAP")
+            {
+                ModelState.Remove("PagadorFlete");
+                ModelState.Remove("TarifaTonelada");
+            }
+
             if (datosUsuario.CentroId == 0)
             {
                 log.Debug("El usuario {0} no tiene seleccionado un centro", datosUsuario.NombreUsuario);
@@ -235,7 +239,7 @@ namespace Molinos.Scato.Web.Controllers
                 if (orden.TipoVehiculoInt == (int)TipoVehiculo.Tren && orden.Cpe && orden.TransportistaTramo2Id != 0)
                 {
                     var transportistaTramo2Id = orden.TransportistaTramo2Id ?? 0;
-                    var resultadoTransportistaTramo2 = SetearTransportista(ref transportistaTramo2Id, orden.TipoComercialId, orden.EsTransportistaTramo2,true);
+                    var resultadoTransportistaTramo2 = SetearTransportista(ref transportistaTramo2Id, orden.TipoComercialId, orden.EsTransportistaTramo2, true);
                     orden.TransportistaTramo2Id = transportistaTramo2Id;
 
                     if (!resultadoTransportistaTramo2)
@@ -430,7 +434,7 @@ namespace Molinos.Scato.Web.Controllers
                     var errorCode = cartaPorteResponse.HayErrores ? cartaPorteResponse.Errores.Keys.First() : "3";
                     var errorMsg = cartaPorteResponse.Errores.Values.FirstOrDefault();
                     var estadoCPE = cartaPorteResponse.Cpe?.EstadoCpe?.ToUpper()?.Trim();
-                    
+
                     if (!cartaPorteResponse.HayErrores && !EstadosCPEdeAFIP.Validos.Contains(estadoCPE))
                     {
                         if (EstadosCPEdeAFIP.Bloqueantes.Any(a => a == cartaPorteResponse.Cpe?.EstadoCpe))
@@ -645,7 +649,6 @@ namespace Molinos.Scato.Web.Controllers
             return ValidarCupoEnSap(orden, usuario, esIngreso);
         }
 
-
         private bool ValidarCupoEnSap(CartaPorteDto orden, DatosUsuario datosUsuario, bool esIngreso)
         {
             try
@@ -748,6 +751,7 @@ namespace Molinos.Scato.Web.Controllers
                 throw;
             }
         }
+
         [DatosUsuario]
         public JsonResult ObtenerTipoVehiculoPorPatenteCPE(long nroCtg, long cuit, DatosUsuario datosUsuario)
         {
@@ -759,10 +763,11 @@ namespace Molinos.Scato.Web.Controllers
             }
             catch (Exception e)
             {
-                log.Error(e, $"No se pudo obtener cpe para ctg { nroCtg } y cuit { cuit }");
+                log.Error(e, $"No se pudo obtener cpe para ctg {nroCtg} y cuit {cuit}");
                 throw;
             }
         }
+
         [DatosUsuario]
         public JsonResult ObtenerImagenCpe(long nroCtg, DatosUsuario datosUsuario)
         {
@@ -846,6 +851,5 @@ namespace Molinos.Scato.Web.Controllers
             }
             return null;
         }
-
     }
 }
--- Molinos.Scato.Web/Scripts/cartaporte.index.js
+++ Molinos.Scato.Web/Scripts/cartaporte.index.js
@@ -27,6 +27,16 @@ jQuery(document).ready(function ($) {
        
     });
 
+
+
+    if (($('#workflow').val() != "1035- SalidaDeMercaderíaConSAP" && $('#workflow').val() != "1035- SalidaDeMercaderíaSinSAP")) {
+
+        $('#PagadorFlete').prop('required', false);
+        $('#PagadorFlete').rules('remove', 'required');
+
+        $('#TarifaTonelada').prop('required', false);
+        $('#TarifaTonelada').rules('remove', 'required');
+    }
     
 
     //Validaciones
