7c22504587acd811d17dae80025a56e1d74e57b9
lidioguedez <lguedez@baufest.com>
2023-07-24 14:21:54 -0300

    Creando Entidades

---
+++ Molinos.Scato.Dominio/Entidades/VariedadMaterial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molinos.Scato.Dominio.Entidades
+{
+    public class VariedadMaterial : IIdentificable
+    {
+        [Key]
+        public virtual int Id { get; set; }
+        [Required]
+        public string Descripcion { get; set; }
+        public string Codigo { get; set; }
+        [Required]
+        public bool Activo { get; set; }
+        [Required]
+        public DateTime FechaCreacion { get; set; }
+        public DateTime? FechaModificacion { get; set; }
+        [Required]
+        public string CreadoPor { get; set; }
+        public string ModificadoPor { get; set; }
+    }
+}
---
+++ Molinos.Scato.Dominio/Entidades/VariedadPorMaterial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molinos.Scato.Dominio.Entidades
+{
+    public class VariedadPorMaterial : IIdentificable
+    {
+        [Key]
+        public virtual int Id { get; set; }
+        public virtual VariedadMaterial VariedadMaterial { get; set; }
+        public virtual Material Material { get; set; }
+
+    }
+}
