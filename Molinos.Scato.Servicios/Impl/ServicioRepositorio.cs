37156ae99cc5b22b50e09552cc2ddb05aa06a804
Manuel Dávila <mdavila@baufest.com>
2023-10-12 13:33:36 -0300

    Merged PR 2661: SCT310-4108 - ANS-3306 - Cambio campo CUIT Chofer por Transportista
    SCT310-4108 - ANS-3306 - Cambio campo CUIT Chofer por Transportista

--- Molinos.Scato.Servicios/Impl/ServicioRepositorio.cs
+++ Molinos.Scato.Servicios/Impl/ServicioRepositorio.cs
@@ -6106,7 +6106,7 @@ namespace Molinos.Scato.Servicios.Impl
                     new DatosInstanciaWorkflowDto
                     {
                         RecorridoId = x.Id,
-                        Cuit = x.Chofer.Cuil,
+                        Cuit = x.Transportista.Cuit,
                         Id = x.InstanciaWorkflow,
                         Material = x.Material.Descripcion,
                         MaterialCodigoSap = x.Material.CodigoSAP,
