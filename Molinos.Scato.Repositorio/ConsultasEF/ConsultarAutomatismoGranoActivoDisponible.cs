0c251bb4a5b5f431213a9debaa640c2907da4cee
Lidio Guedez <lguedez@baufest.com>
2024-01-09 12:20:49 -0300

    Se modifica la consulta
    Se modifica la consulta para validar que solo se tome los automatismos activos

--- Molinos.Scato.Repositorio/ConsultasEF/ConsultarAutomatismoGranoActivoDisponible.cs
+++ Molinos.Scato.Repositorio/ConsultasEF/ConsultarAutomatismoGranoActivoDisponible.cs
@@ -96,7 +96,7 @@ namespace Molinos.Scato.Repositorio.ConsultasEF
 
         private List<AutomatismoGrano> ListarAutomatismosActivosConMismasCaracteristicas(DbContext contexto)
         {
-            Func<AutomatismoGrano, bool> FiltrarAutomatismoActivoConMismoMaterialYVariedad = x => x.MaterialId == this.materialId && x.TipoVariedadId == this.tipoVariedadId;
+            Func<AutomatismoGrano, bool> FiltrarAutomatismoActivoConMismoMaterialYVariedad = x => x.Activo && x.MaterialId == this.materialId && x.TipoVariedadId == this.tipoVariedadId;
 
             var automatismosGranos = contexto.Set<AutomatismoGrano>()
                                             .Include(x => x.Hidraulicas)
