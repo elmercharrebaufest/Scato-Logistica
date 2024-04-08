c9a2ba0166d5c4b7523f658f9c828f632f27c506
mdavila <mdavila@baufest.com>
2024-04-08 15:16:55 -0500

    SCT310-4702 - ANS-3639 - Logoff scatomobile

---
+++ Molinos.Scato.WebMobile/Controllers/UsuarioController.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Molinos.Scato.WebMobile.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult SignOut()
+        {
+            var adfsLogoutUrl = "https://bfdev271.baunet.local/adfs/ls/?wa=wsignout1.0";
+            return Redirect(adfsLogoutUrl);
+        }
+    }
+}
