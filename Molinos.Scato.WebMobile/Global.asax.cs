9c2ca72052744734bf8afdd84cb152ccf85d87b1
Hernan Lavrencic <hlavrencic@baufest.com>
2024-10-07 17:44:13 -0300

    Quito contenido del Application_PostAuthenticateRequest

--- Molinos.Scato.WebMobile/Global.asax.cs
+++ Molinos.Scato.WebMobile/Global.asax.cs
@@ -39,12 +39,7 @@ namespace Molinos.Scato.WebMobile
 
         protected void Application_PostAuthenticateRequest(object sender, EventArgs e)
         {
-            var cookie = Response.Cookies["FedAuth"];
-            if (cookie != null)
-            {
-                // Set the SameSite attribute to "Strict", "Lax", or "None"
-                cookie.SameSite = SameSiteMode.Lax;
-            }
+
         }
     }
 }
