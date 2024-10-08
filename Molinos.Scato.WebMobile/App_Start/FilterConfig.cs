8fe140ce56e1c47a332af0ebd2d46b33de6b323a
Hernan Lavrencic <hlavrencic@baufest.com>
2024-10-07 23:14:29 -0300

    Error en certificado

--- Molinos.Scato.WebMobile/App_Start/FilterConfig.cs
+++ Molinos.Scato.WebMobile/App_Start/FilterConfig.cs
@@ -7,6 +7,7 @@ namespace Molinos.Scato.WebMobile.App_Start
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
+            filters.Add(new CertificateHandleErrorAttribute());
             filters.Add(new HandleErrorAttribute());
             filters.Add(new AvoidCacheFilterAttribute());
         }
