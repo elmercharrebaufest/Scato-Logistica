b02ee63fe818de3af818d412028299956bd3fb99
Lidio Guedez <lguedez@baufest.com>
2024-10-31 14:53:44 -0300

    fix

--- Molinos.Scato.Web/Scripts/ordenCargaInternaFason.js
+++ Molinos.Scato.Web/Scripts/ordenCargaInternaFason.js
@@ -1,9 +1,5 @@
 
 $(document).ready(function () {
-    $('#LocalidadDestinoId').removeAttr('data-val');
-    $('#LocalidadDestinoId').removeAttr('data-val-number');
-    $('#LocalidadDestinoId').removeAttr('data-val-required');
-
 
     if ($("input[name='cargaCupoIdValue']").val() === "true") {
         $('.btn.btn-primary[type="submit"]').focus();
